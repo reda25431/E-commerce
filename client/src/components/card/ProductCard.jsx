7714ae68dc031a810dd3391d1d9cd20c88cfb50c
Reda_Peeraphat <peeraphats2000@gmail.com>
2025-05-12 22:17:42 +0700

    motion shop page

--- client/src/components/card/ProductCard.jsx
+++ client/src/components/card/ProductCard.jsx
@@ -2,33 +2,44 @@ import React from 'react'
 import { ShoppingCart } from 'lucide-react';
 import useEcomStore from '../../store/Ecom-store';
 import { numberFormat } from '../../utils/number';
+import { motion } from "motion/react"
 
 const ProductCard = ({ item }) => {
     const actionAddtoCart = useEcomStore((state) => state.actionAddtoCart)
 
     return (
-        <div className='border rounded-md shadow-md p-2 w-36 md:w-48'>
-            <div>
-                {
-                    item.images && item.images.length > 0
-                        ? <img className='w-full h-32 object-cover hover:scale-105 hover:duration-200 rounded-md shadow' src={item.images[0].url} />
-                        : <div className='w-full h-32 rounded-md text-center flex items-center justify-center shadow bg-gray-200'>No Image</div>
-                }
+        <motion.div
+            initial={{ opacity: 0, scale: 0.5 }}
+            animate={{ opacity: 1, scale: 1 }}
+            transition={{
+                duration: 0.5,
+                delay: 0.5,
+                ease: [0, 0.71, 0.2, 1.01],
+            }}
+        >
+            <div className='border rounded-md shadow-md p-2 w-36 md:w-48'>
+                <div>
+                    {
+                        item.images && item.images.length > 0
+                            ? <img className='w-full h-32 object-cover hover:scale-105 hover:duration-200 rounded-md shadow' src={item.images[0].url} />
+                            : <div className='w-full h-32 rounded-md text-center flex items-center justify-center shadow bg-gray-200'>No Image</div>
+                    }
+                </div>
+                <div className='py-2'>
+                    <p className='text-md truncate'>{item.title}</p>
+                    <p className='text-xs text-gray-500 truncate'>{item.description}</p>
+                </div>
+                <div className='flex justify-between items-center'>
+                    <span className='text-sm font-bold'>฿{numberFormat(item.price)}</span>
+                    <button
+                        className='border text-[#002244] cursor-pointer rounded-md p-2 hover:bg-[#003366] hover:text-white'
+                        onClick={() => actionAddtoCart(item)}
+                    >
+                        <ShoppingCart />
+                    </button>
+                </div>
             </div>
-            <div className='py-2'>
-                <p className='text-md truncate'>{item.title}</p>
-                <p className='text-xs text-gray-500 truncate'>{item.description}</p>
-            </div>
-            <div className='flex justify-between items-center'>
-                <span className='text-sm font-bold'>฿{ numberFormat(item.price) }</span>
-                <button
-                    className='border text-[#002244] cursor-pointer rounded-md p-2 hover:bg-[#003366] hover:text-white'
-                    onClick={()=>actionAddtoCart(item)}
-                >
-                    <ShoppingCart />
-                </button>
-            </div>
-        </div>
+        </motion.div>
     )
 }
 
